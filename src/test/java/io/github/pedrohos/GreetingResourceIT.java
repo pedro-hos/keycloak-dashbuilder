d68797f0933763d2b3dcdc4d2ad06fb5bb48861e
Pedro Hos <pedro-hos@outlook.com>
2023-02-20 18:03:13 -0300

    removing project

--- src/test/java/io/github/pedrohos/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package io.github.pedrohos;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}

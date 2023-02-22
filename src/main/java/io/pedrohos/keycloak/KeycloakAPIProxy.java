e6ea546c8bb11dd256ea64d47341f4efcf73970f
Pedro Hos <pedro-hos@outlook.com>
2023-02-22 18:06:34 -0300

    getting data from REST API

--- src/main/java/io/pedrohos/keycloak/KeycloakAPIProxy.java
+++ src/main/java/io/pedrohos/keycloak/KeycloakAPIProxy.java
@@ -1,13 +1,18 @@
 package io.pedrohos.keycloak;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 
+import io.quarkus.oidc.AccessTokenCredential;
 import jakarta.annotation.security.PermitAll;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -16,17 +21,53 @@ import jakarta.ws.rs.QueryParam;
 @PermitAll
 @Path("/api")
 public class KeycloakAPIProxy {
+    
+    private static final Logger LOG = Logger.getLogger(KeycloakAPIProxy.class);
 
-    @ConfigProperty(name = "api.base-path")
-    private String basePath;
-
-
+    @ConfigProperty(name = "api.keycloak.base-url")
+    private String keycloakBaseUrl;
+    
+    @Inject
+    AccessTokenCredential accessTokenCredential;
 
     @GET
     @Produces("application/json")
     public String request(@QueryParam("path") String path) throws IOException {
-        // TODO:  call the path on the keycloak REST api and return the JSON        
-        return Files.readString(Paths.get(basePath, path + ".json"));
+        return getInfo(path);
+    }
+
+    public String getInfo(final String path) {
+        
+        StringBuilder body = new StringBuilder();
+        
+        try {
+
+            URL url = new URL(keycloakBaseUrl + path);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Authorization", "Bearer " + accessTokenCredential.getToken());
+
+            if (conn.getResponseCode() != 200) {
+                LOG.error("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            String output;
+            
+            while ((output = br.readLine()) != null) {
+                body.append(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            LOG.error("Failed during retrieve REST API: ", e);
+        } catch (IOException e) {
+            LOG.error("Failed during retrieve REST API : ", e);
+        }
+        
+        return body.toString();
         
     }
 
